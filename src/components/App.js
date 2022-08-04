7e276ac80bc57c29795b4f20eddff02ed3c79f70
Ayush Dixit <43095344+dixitsh@users.noreply.github.com>
2022-08-04 13:34:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,76 +1,48 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
+const list = [{ name: 'Goa', country: 'India' },
+{ name: 'Amsterdam', country: 'Netherlands' },
+{ name: 'New York', country: 'USA' },
+{ name: 'Darjeeling', country: 'India' },
+{ name: 'Tokyo', country: 'Japan' },
+{ name: 'Lonavala', country: 'India' },
+{ name: 'Brandenburg Gate', country: 'Germany' },
+{ name: 'Reichstag Building', country: 'Germany' },
+{ name: 'Museum Island', country: 'Germany' },
+{ name: 'Munnar', country: 'India' },
+{ name: 'Leh Ladakh', country: 'India' },
+{ name: 'Agra', country: 'India' },
+{ name: 'Dalhousie', country: 'India' },
+{ name: 'Coorg', country: 'India' },
+{ name: 'Rome', country: 'Italy' },
+{ name: 'Milan', country: 'Italy' },
+{ name: 'Venice', country: 'Italy' },
+{ name: 'Varanasai', country: 'India' },
+{ name: 'Jaipur', country: 'India' },
+{ name: 'The Hofburg', country: 'Austria' },
+{ name: 'Belvedere Palace', country: 'Austria' },
+{ name: 'St. Stephen Cathedral', country: 'Austria' },
+{ name: 'Kahna National Park', country: 'India' },
+{ name: 'Amritsar', country: 'India' },
+{ name: 'Mussoorie', country: 'India' },
+{ name: 'Mount Abu', country: 'India' },
+{ name: 'Tirupati', country: 'India' },
+]
 
-class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			list: [
-				{
-					name: "naman",
-					percent: 60.0000
-				},
-				{
-					name: "ritesh",
-					percent: 99.9431
-				},
-				{
-					name: "hitesh",
-					percent: 65.3365
-				},
-				{
-					name: "shreya",
-					percent: 80.6400
-				},
-				{
-					name: "ankur",
-					percent: 78.8970
-				},
-				{
-					name: "vanshu",
-					percent: 78.9876
-				},
-				{
-					name: "krupal",
-					percent: 85.3214
-				},
-			],
-		}
-	};
-	render() {
-		return (
-			<div id="main">
-				{
-					//Correct the percentage conditional for correct output
-					this.state.list.map(function (item, index) {
-						if (item.percent >= 90) {
-							return (
-								<div className="bg-pink" key={index}>
-									<div className="name">
-										{item.name}
-									</div>
-									<div className="percent">
-										{item.percent.toFixed(2)}
-									</div>
-								</div>
-							)
-						} else if (item.percent > 75) {
-							return (
-								<div className="" key={index}>
-									<div className="name">
-										{item.name}
-									</div>
-									<div className="percent">
-										{item.percent.toFixed(2)}
-									</div>
-								</div>
-							)
-						}
-					})
-				}
-			</div >
-		);
-	}
+const App = () => {
+  return (
+    <div id="main">
+      <ol>
+        {
+          list.filter((data) => data.country === 'India').filter((data) => data.name === 'Goa' || data.name === 'Darjeeling' || data.name === 'Lonavala').map((data, index) => {
+            return (
+              <li key={`location${index + 1}`}>{data.name}</li>
+            )
+          })
+        }
+      </ol>
+    </div>
+  )
 }
 
 
